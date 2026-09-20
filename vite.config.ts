24b0377ecff6d61a91f18b89c343fd9fd705b9ec
AlenaVP Legion <poprugo@gmail.com>
2026-09-20 19:43:21 +0200

    chore: configure a bundler Vite
    [npm install -D @types/node@22]

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+
+export default defineConfig({
+  base: '/minigames/',
+  resolve: {
+    alias: {
+      '@app': fileURLToPath(new URL('./src/app', import.meta.url)),
+      '@widgets': fileURLToPath(new URL('./src/app/widgets', import.meta.url)),
+      '@shared': fileURLToPath(new URL('./src/app/shared', import.meta.url)),
+      '@dialogs': fileURLToPath(new URL('./src/app/dialogs', import.meta.url)),
+      '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler',
+        loadPaths: ['src/styles/abstracts'],
+      },
+    },
+  },
+  build: { outDir: 'dist' },
+  server: { open: true },
+});
