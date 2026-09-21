cd3a1aeaf89219593d84a53dfbd6291ee7121190
AlenaVP Legion <poprugo@gmail.com>
2026-09-22 00:11:55 +0200

    feat: add a section hero

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,9 +1,13 @@
 import { ComponentBase } from '@app/core/component.base';
+import { HeroSection } from './components/hero-section';
 
 export class HomePage extends ComponentBase {
   protected render(): HTMLElement {
     const page = document.createElement('div');
     page.classList.add('home-page');
+
+    const hero = new HeroSection();
+    hero.mount(page);
 
     return page;
   }
