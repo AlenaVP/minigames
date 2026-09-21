160fe8a1fe6c812d1e396a9ef7a12c06bc94c598
AlenaVP Legion <poprugo@gmail.com>
2026-09-21 16:00:40 +0200

    feat: add base styles

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'modern-normalize/modern-normalize.css';
 import './styles/global.scss';
 import { bootstrapApp } from '@app/core/app';
 
--- vite.config.ts
+++ vite.config.ts
@@ -16,8 +16,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        api: 'modern-compiler',
-        loadPaths: ['src/styles/abstracts'],
+        loadPaths: ['src/styles'],
       },
     },
   },
