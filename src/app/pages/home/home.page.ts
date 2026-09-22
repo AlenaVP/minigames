be1b47193172107c7f47884dcf9d55a812636964
AlenaVP Legion <poprugo@gmail.com>
2026-09-23 00:20:54 +0200

    feat: add a section top-players

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,21 +1,18 @@
 import { ComponentBase } from '@app/core/component.base';
-import { DeveloperCtaSection } from './components/developer-cta-section';
-import { NewGamesSection } from './components/new-games-section';
 import { HeroSection } from './components/hero-section';
+import { NewGamesSection } from './components/new-games-section';
+import { TopPlayersSection } from './components/top-players-section';
+import { DeveloperCtaSection } from './components/developer-cta-section';
 
 export class HomePage extends ComponentBase {
   protected render(): HTMLElement {
     const page = document.createElement('div');
     page.classList.add('home-page');
 
-    const hero = new HeroSection();
-    hero.mount(page);
-
-    const newGames = new NewGamesSection();
-    newGames.mount(page);
-
-    const developerCta = new DeveloperCtaSection();
-    developerCta.mount(page);
+    new HeroSection().mount(page);
+    new NewGamesSection().mount(page);
+    new TopPlayersSection().mount(page);
+    new DeveloperCtaSection().mount(page);
 
     return page;
   }
