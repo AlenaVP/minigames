fdb4610f48b63aeba30bfc90d75b00d43cf8176e
AlenaVP Legion <poprugo@gmail.com>
2026-09-22 20:06:47 +0200

    feat: add a section developer-cta

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { ComponentBase } from '@app/core/component.base';
+import { DeveloperCtaSection } from './components/developer-cta-section';
 import { HeroSection } from './components/hero-section';
 
 export class HomePage extends ComponentBase {
@@ -8,6 +9,9 @@ export class HomePage extends ComponentBase {
 
     const hero = new HeroSection();
     hero.mount(page);
+
+    const developerCta = new DeveloperCtaSection();
+    developerCta.mount(page);
 
     return page;
   }
